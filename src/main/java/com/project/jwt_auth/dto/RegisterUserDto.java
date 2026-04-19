11460c631969b03a15a0cc8c24bc90161eba8101
Vijay Kumar <lvijaykumar4@gmail.com>
2026-04-19 12:52:04 +0530

    Login and Register user class added.

---
+++ src/main/java/com/project/jwt_auth/dto/RegisterUserDto.java
@@ -0,0 +1,14 @@
+package com.project.jwt_auth.dto;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RegisterUserDto
+{
+    private String email;
+    private String password;
+    private String fullName;
+}
